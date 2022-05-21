fb71962ba4dab6fa41a74662d3539b6f648a2187
borismassesa <bmmassesa@gmail.com>
2022-05-20 20:25:06 -0700

    new changes

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const btn = document.querySelector('button');
+const h1 = document.querySelector('h1');
+
 btn.addEventListener('click',function () {
-  document.body.style.backgroundColor = 'olive';
+  const newColor = makeRandomColor();
+  document.body.style.backgroundColor = newColor;
+  h1.innertext = newColor;
+})
+
+const makeRandomColor = () => {
   const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
-  const newColor = `rgb(${r}, ${g} , ${b})`
-  document.body.style.backgroundColor = newColor;
-})
+  return newColor = `rgb(${r}, ${g} , ${b})`
+}
